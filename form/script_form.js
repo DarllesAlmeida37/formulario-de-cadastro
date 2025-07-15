53ea01c778eab679a4bc91723d4f8d5dc48465c5
Darlles Almeida <darllesalmeida37@gmail.com>
2025-07-15 15:50:21 -0300

    Adicionei javascript no formulário!
    manipulei o DOM com javascript.

--- form/script_form.js
+++ form/script_form.js
@@ -3,15 +3,23 @@ function cadastrar(){
     const loja = document.getElementById('loja')
     const cadastrado = document.getElementById('cadastrado')
     const img = document.getElementById('imagem')
-    let dados = []
+    let dadosNomes = []
+    let dadosLoja = []
 
     if(nome.value == 0 || loja.value == 0){
         alert('Campo "NOME" e "LOJA" são obrigatórios!')
     } else{
         img.remove()
-        dados.push(nome.value, loja.value)
+        dadosNomes.push(nome.value)
+        dadosLoja.push(loja.value)
         const resultado = document.createElement('h2')
         resultado.textContent = 'Vendedor cadastrado com sucesso!'
         cadastrado.appendChild(resultado)
+        cadastrado.innerHTML += `<br><br>VENDEDOR: ${dadosNomes}`
+        cadastrado.innerHTML += `<p>Loja: ${dadosLoja}</p>`
     }
+    
+    nome.value = ''
+    nome.focus()
+    loja.value = ''
 }
