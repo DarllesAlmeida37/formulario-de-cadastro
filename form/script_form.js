97c714b0269a29ac587431319076b39af8814593
Darlles Almeida <darllesalmeida37@gmail.com>
2025-07-15 16:20:10 -0300

    Manipulei os dados do usuário.
    Conseguir exibir os dados que o usuário inseriu, mostrei na tela o cadastro realizado!

--- form/script_form.js
+++ form/script_form.js
@@ -1,7 +1,7 @@
 function cadastrar(){
     const nome = document.getElementById('nome')
     const loja = document.getElementById('loja')
-    const cadastrado = document.getElementById('cadastrado')
+    const VendCadastrado = document.getElementById('cadastrado')
     const img = document.getElementById('imagem')
     let dadosNomes = []
     let dadosLoja = []
@@ -9,14 +9,21 @@ function cadastrar(){
     if(nome.value == 0 || loja.value == 0){
         alert('Campo "NOME" e "LOJA" são obrigatórios!')
     } else{
-        img.remove()
+        if(img){
+            img.remove()
+            const resultado = document.createElement('h2')
+            resultado.textContent = `Lista de Vendedores Cadastrados!`
+            resultado.style.marginBottom = '30px'
+            VendCadastrado.appendChild(resultado)
+        }
+        VendCadastrado.innerHTML += ''
+        VendCadastrado.style.border = '2px solid rgb(28, 85, 158)'
+        VendCadastrado.style.padding = '30px'
+        VendCadastrado.style.borderRadius = '10px'
         dadosNomes.push(nome.value)
         dadosLoja.push(loja.value)
-        const resultado = document.createElement('h2')
-        resultado.textContent = 'Vendedor cadastrado com sucesso!'
-        cadastrado.appendChild(resultado)
-        cadastrado.innerHTML += `<br><br>VENDEDOR: ${dadosNomes}`
-        cadastrado.innerHTML += `<p>Loja: ${dadosLoja}</p>`
+        
+        VendCadastrado.innerHTML += `<p>VENDEDOR: ${dadosNomes} - Loja: ${dadosLoja}</p>`
     }
     
     nome.value = ''
