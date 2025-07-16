302467e201910f8c00542a520b717ff684baf251
Darlles Almeida <darllesalmeida37@gmail.com>
2025-07-16 16:20:59 -0300

    Criei um arrey para salvar os dados de contato do usuário.
    Criei um arrey para salvar e manipular os dados de contato do usuário, mas ainda não concluí!

--- form/script_form.js
+++ form/script_form.js
@@ -1,10 +1,14 @@
 function cadastrar(){
     const nome = document.getElementById('nome')
+    const tel = document.getElementById('telefone')
+    const email = document.getElementById('email')
     const loja = document.getElementById('loja')
     const VendCadastrado = document.getElementById('cadastrado')
     const img = document.getElementById('imagem')
     let dadosNomes = []
     let dadosLoja = []
+    let dasdosContado = []
+
 
     if(nome.value == 0 || loja.value == 0){
         alert('Campo "NOME" e "LOJA" são obrigatórios!')
@@ -17,16 +21,18 @@ function cadastrar(){
             VendCadastrado.appendChild(resultado)
         }
         VendCadastrado.innerHTML += ''
-        VendCadastrado.style.border = '2px solid rgb(28, 85, 158)'
+        VendCadastrado.style.border = '2px solid rgba(85, 144, 221, 0.75)'
         VendCadastrado.style.padding = '30px'
         VendCadastrado.style.borderRadius = '10px'
         dadosNomes.push(nome.value)
         dadosLoja.push(loja.value)
         
-        VendCadastrado.innerHTML += `<p>VENDEDOR: ${dadosNomes} - Loja: ${dadosLoja}</p>`
+        VendCadastrado.innerHTML += `<p><strong>VENDEDOR:</strong> ${dadosNomes} - <strong>Loja:</strong> ${dadosLoja}</p>`
     }
     
     nome.value = ''
     nome.focus()
     loja.value = ''
+    tel.value = ''
+    email.value = ''
 }
