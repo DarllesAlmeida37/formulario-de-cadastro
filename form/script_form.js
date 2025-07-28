650b7914212209ee76f292146aa94d9009b563eb
Darlles Almeida <darllesalmeida37@gmail.com>
2025-07-28 14:31:50 -0300

    Criei uma confirmação de cadastro.
    Criei uma confirmação de cadastro para o usuário verificar os dados inseridos e confirmar o cadastro.

--- form/script_form.js
+++ form/script_form.js
@@ -18,29 +18,37 @@ function cadastrar(){
     if(nome.value.trim() === '' || loja.value.trim() === '' || email.value.trim() === '' || tel.value.trim() === "" || jaCadastrado(nome.value, dadosNomes)){
         alert('Dados inválidos ou o Vendedor já foi cadastrado!')
     } else{
-        if(img){ //fiz uma verifição da imagem para não repetir os dados, nem o título, caso o usuário queira cadastrar mais de uma pessoa.
-            img.remove() // Depois do primeiro cadastro, a verificação vai ser (false), então ela nao vai repetir o título.
-            const resultado = document.createElement('h2')
-            resultado.textContent = `Lista de Vendedores Cadastrados!`
-            resultado.style.marginBottom = '30px'
-            VendCadastrado.appendChild(resultado)
-        }
-        const novoContato = {
-            telefone: tel,
-            email: email
-        }
-        dadosContato.push(novoContato) // Salvei os dados de contato (telefone e email) para caso o beck-end precise comparar os dados!
-        console.log(dadosContato) // Exibi os dados no console só para ter certeza de que o código funcionou!
 
-        dadosNomes.push(nome.value) // Salvei o nome e loja separados, só para exibir para o usuário na tela!
-        dadosLoja.push(loja.value)
-        VendCadastrado.innerHTML += ''
-        VendCadastrado.style.border = '2px solid #5590ddbf'
-        VendCadastrado.style.padding = '30px'
-        VendCadastrado.style.borderRadius = '10px'
+        const confirmacao = confirm(`Deseja salvar o Vendedor(a): ${nome.value}\nLoja: ${loja.value}`)
+
+        if (confirmacao){
+                  
+            if(img){ //fiz uma verifição da imagem para não repetir os dados, nem o título, caso o usuário queira cadastrar mais de uma pessoa.
+                img.remove() // Depois do primeiro cadastro, a verificação vai ser (false), então ela nao vai repetir o título.
+                const resultado = document.createElement('h2')
+                resultado.textContent = `Lista de Vendedores Cadastrados!`
+                resultado.style.marginBottom = '30px'
+                VendCadastrado.appendChild(resultado)
+            }
+
+            const novoContato = {
+                telefone: tel,
+                email: email
+            }
+            dadosContato.push(novoContato) // Salvei os dados de contato (telefone e email) para caso o beck-end precise comparar os dados!
+            console.log(dadosContato) // Exibi os dados no console só para ter certeza de que o código funcionou!
+
+            dadosNomes.push(nome.value) // Salvei o nome e loja separados, só para exibir para o usuário na tela!
+            dadosLoja.push(loja.value)
+            VendCadastrado.innerHTML += ''
+            VendCadastrado.style.border = '2px solid #5590ddbf'
+            VendCadastrado.style.padding = '30px'
+            VendCadastrado.style.borderRadius = '10px'
 
         
-        VendCadastrado.innerHTML += `<p><strong>VENDEDOR:</strong> ${dadosNomes} - <strong>Loja:</strong> ${dadosLoja}</p>`
+            VendCadastrado.innerHTML += `<p><strong>VENDEDOR:</strong> ${dadosNomes} - <strong>Loja:</strong> ${dadosLoja}</p>`  
+        }
+
     }
     
     nome.value = ''
@@ -49,3 +57,8 @@ function cadastrar(){
     email.value = ''
     nome.focus()
 }
+
+// Lembrar de fazer um array para cada input do cadastro.
+// ex: dadosNome, dadosTelefone, dadosEmail, dadosLoja.
+// Depois, mostrar todos os dados na tela para o usuário.
+// Obs: depois de criar todos os arrays, lembrar de apagar o array "dadosContato" com o objeto "novoContato" que está dentro dele.
